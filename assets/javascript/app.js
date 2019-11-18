b4701a615c6672634e587a8c836cd3372de044de
Bradley Knutson <bradley@bknutson.com>
2019-11-18 16:45:30 -0700

    Limited search to US

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,18 @@
-var city;
-var postcode;
-var state;
-var country;
-var lat;
-var lon;
-
+var firebaseConfig = {
+    apiKey: "AIzaSyDlFw-SrUXQdgRDqUvTkPZcwQm-tgIIgAw",
+    authDomain: "hipsters-paradise.firebaseapp.com",
+    databaseURL: "https://hipsters-paradise.firebaseio.com",
+    projectId: "hipsters-paradise",
+    storageBucket: "hipsters-paradise.appspot.com",
+    messagingSenderId: "343184641394",
+    appId: "1:343184641394:web:45d963e7f2ac119f2421eb"
+};
+firebase.initializeApp(firebaseConfig);
+database = firebase.database();
 
 
 function getMapData(search) {
-    var url = "https://nominatim.openstreetmap.org/?format=json&limit=1&addressdetails=1&q="
+    var url = "https://nominatim.openstreetmap.org/?format=json&limit=1&addressdetails=1&countrycodes=US&q="
     var queryTerm = '';
     for(let i = 0; i<search.length; i++){
         if(search[i] === ' '){
@@ -23,16 +27,25 @@ function getMapData(search) {
         success: function (response) {
             if(response[0]){
                 console.log(response);
-                city = response[0].address.city;
-                postcode = response[0].address.postcode;
-                state = response[0].address.state;
-                country = response[0].address.country;
-                lat = response[0].lat;
-                lon = response[0].lon;
+                var city = response[0].address.city;
+                //var postcode = response[0].address.postcode;
+                var state = response[0].address.state;
+                var country = response[0].address.country;
+                var lat = response[0].lat;
+                var lon = response[0].lon;
             }else{
                 console.log(response);
                 console.log('Incorrect search');
             }
+            database.ref('search').push({
+                searchTerm: search,
+                city: city,
+                //postcode: postcode,
+                state: state,
+                country: country,
+                lat: lat,
+                lon: lon
+            })
         },
         error: function (xhr, ajaxOptions, thrownError) {
             console.log(xhr.status);
@@ -48,3 +61,4 @@ $('#getLocation').on('click', function(){
         getMapData(position.coords.latitude + ',' + position.coords.longitude); 
     });
 })
+

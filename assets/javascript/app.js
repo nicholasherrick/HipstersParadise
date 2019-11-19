88170725663dc3ee4af57b50947ba8959fed74bd
Kevin Brown <48394009+kbrowngithub@users.noreply.github.com>
2019-11-19 09:19:34 -0700

    Enabled city/state input in html, added table in html, added ticketmaster api logic, added error handling in getMapData

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,10 +14,10 @@ database = firebase.database();
 function getMapData(search) {
     var url = "https://nominatim.openstreetmap.org/?format=json&limit=1&addressdetails=1&countrycodes=US&q="
     var queryTerm = '';
-    for(let i = 0; i<search.length; i++){
-        if(search[i] === ' '){
+    for (let i = 0; i < search.length; i++) {
+        if (search[i] === ' ') {
             queryTerm += '+';
-        }else{
+        } else {
             queryTerm += search[i].toLowerCase();
         }
     }
@@ -25,25 +25,28 @@ function getMapData(search) {
         type: "GET",
         url: url + queryTerm,
         success: function (response) {
-            if(response[0].address.city){
+            if (response[0] !== undefined && response[0].address.city) {
                 console.log(response);
                 var city = response[0].address.city;
                 //var postcode = response[0].address.postcode;
                 var state = response[0].address.state;
                 var lat = response[0].lat;
                 var lon = response[0].lon;
-            }else{
+
+                database.ref('search').push({
+                    searchTerm: search,
+                    city: city,
+                    //postcode: postcode,
+                    state: state,
+                    lat: lat,
+                    lon: lon
+                });
+                findSuggest(lat + "," + lon);
+            } else {
                 console.log(response);
                 console.log('Incorrect search');
+                findSuggest(0);
             }
-            database.ref('search').push({
-                searchTerm: search,
-                city: city,
-                //postcode: postcode,
-                state: state,
-                lat: lat,
-                lon: lon
-            })
         },
         error: function (xhr, ajaxOptions, thrownError) {
             console.log(xhr.status);
@@ -52,11 +55,60 @@ function getMapData(search) {
     });
 }
 
-
-
-$('#getLocation').on('click', function(){
-    navigator.geolocation.getCurrentPosition(function(position) {
-        getMapData(position.coords.latitude + ',' + position.coords.longitude); 
+$('#getLocation').on('click', function () {
+    navigator.geolocation.getCurrentPosition(function (position) {
+        getMapData(position.coords.latitude + ',' + position.coords.longitude);
     });
-})
+});
+
+$("#search").keypress(function (event) {
+    console.log("Key code = " + event.which);
+    if (event.which == 13) {
+        event.preventDefault();
+        $("tbody").empty();
+        getMapData($("#search").val());
+    }
+});
+
+function findSuggest(coordinates) {
+    if (coordinates === 0) {
+        showVenues(0);
+    }
+    else {
+        $.ajax({
+            type: "GET",
+            url: "https://app.ticketmaster.com/discovery/v2/suggest.json?latlong=" + coordinates + "&apikey=G8wASZPn3DFcYGef4xr5K2DUzqvDxQJ2",
+            async: true,
+            dataType: "json",
+            success: function (json) {
+                console.log(JSON.stringify(json));
+                showVenues(json);
+            },
+            error: function (xhr, status, err) {
+                console.log(err);
+            }
+        });
+    }
+}
+
+function showVenues(json) {
+    if (json !== 0 && json._embedded.venues !== undefined) {
+        var events = json._embedded.venues;
+        console.log(events);
+
+        for (var i = 0; i < events.length; i++) {
+            console.log(JSON.stringify(events[i]));
+            var newRow = $("<tr>").append(
+                $("<td><a href=\"" + events[i].url + "\" style=\"display:block;\">" + events[i].name + "</a></td>")
+            );
+            $("#events > tbody").append(newRow);
+        }
+    }
+    else {
+        var newRow = $("<tr>").append(
+            $("<td>0 Events Found</td>")
+        );
+        $("#events > tbody").append(newRow);
+    }
+}
 

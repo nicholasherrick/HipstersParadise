6e876836cb3583e1d69148ef0ae178cf796be218
Nick <54647798+nicholasherrick@users.noreply.github.com>
2019-11-20 21:58:22 -0500

    Added functionality for brewrey API (#23)
    * added open brewrey api functionality based on a search by city
    
    * ?
    
    * added open brewrey api functionality based on a search by city
    
    * ?
    
    * got function working and pulling the city from location data. brewrey info appends to right side card. added .trim, .split, and .join to getMapData search bar entry to allow for searching of places with a space in the title such as new york
    
    * removed .split .join
    
    * added a .empty to empty the table each search
    
    * Typos

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,6 +16,7 @@ var currentSearch;
 
 function getMapData(search) {
     $("#events > tbody").empty();
+    $("#brewreys > tbody").empty();
     var url = "https://nominatim.openstreetmap.org/?format=json&limit=1&addressdetails=1&countrycodes=US&q="
     var queryTerm = '';
     for (let i = 0; i < search.length; i++) {
@@ -44,6 +45,7 @@ function getMapData(search) {
                     lon: lon
                 });
                 findSuggest(lat + "," + lon);
+                getBreweriesByCity(city);
             } else {
                 console.log(response);
                 console.log('Invalid search');
@@ -114,6 +116,27 @@ function showVenues(json) {
     }
 }
 
+function getBreweriesByCity(city) {
+    var queryURL = "https://api.openbrewerydb.org/breweries?by_city=" + city + "&page=1&per_page=5";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+        for (var i = 0; i < response.length; i++) {
+            var newRow1 = $("<tr>").append(
+                $("<td><a href=\"" + response[i].website_url + "\" style=\"display:block;\">" + response[i].name + "</a></td>")
+            );
+            var newRow2 = $("<tr>").append(
+                $("<td>" + response[i].street + " " + response[i].postal_code + "</td>")
+            );
+            var newRow3 = $("<tr>").append(
+                $("<td>" + response[i].phone + "</td>")
+            );
+            $("#brewerys").append(newRow1, newRow2, newRow3);
+        }
+    });
+};
 
 database.ref('.info/connected').on('value', function(snapshot){
     if(snapshot.val() && !localStorage.getItem('userkey')){

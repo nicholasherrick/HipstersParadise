f6be2c888bc91ff31f0a4d1cd94f6a1dd75bcfcc
Nick Herrick <nth.herrick@gmail.com>
2019-11-21 18:34:19 -0500

    fixed issue where breweries with no website reloaded page. breweries with no website now no longer show up as links

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -124,16 +124,29 @@ function getBreweriesByCity(city) {
     }).then(function(response) {
         console.log(response);
         for (var i = 0; i < response.length; i++) {
-            var newRow1 = $("<tr>").append(
-                $("<td><a href=\"" + response[i].website_url + "\" style=\"display:block;\">" + response[i].name + "</a></td>")
-            );
-            var newRow2 = $("<tr>").append(
-                $("<td>" + response[i].street + " " + response[i].postal_code + "</td>")
-            );
-            var newRow3 = $("<tr>").append(
-                $("<td>" + response[i].phone + "</td>")
-            );
-            $("#brewreys").append(newRow1, newRow2, newRow3);
+            if (response[i].website_url === undefined || response[i].website_url.length == 0) {
+                var newRow1 = $("<tr>").append(
+                    $("<td>" + response[i].name + " (no website)</td>")
+                );
+                var newRow2 = $("<tr>").append(
+                    $("<td>" + response[i].street + " " + response[i].postal_code + "</td>")
+                );
+                var newRow3 = $("<tr>").append(
+                    $("<td>" + response[i].phone + "</td>")
+                );
+                $("#brewreys").append(newRow1, newRow2, newRow3);
+            } else {
+                var newRow1 = $("<tr>").append(
+                    $("<td><a href=\"" + response[i].website_url + "\" style=\"display:block;\">" + response[i].name + "</a></td>")
+                );
+                var newRow2 = $("<tr>").append(
+                    $("<td>" + response[i].street + " " + response[i].postal_code + "</td>")
+                );
+                var newRow3 = $("<tr>").append(
+                    $("<td>" + response[i].phone + "</td>")
+                );
+                $("#brewreys").append(newRow1, newRow2, newRow3);
+            }
         }
     });
 };

b2157552ae3a472f09cc891f5747a2e6bcd0ef9c
Nick Herrick <nth.herrick@gmail.com>
2019-11-21 17:50:20 -0500

    fixed all instances of breweries being spelled wrong, added some more text in html elements and javascript dynamic html elements to give the user a clearer picture of whats going on, changed color of links and gave them color change on hover property in the css so that its more obvious they are actually links. nes feel free to mess with the specifics on this one

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,7 +15,7 @@ var currentSearch;
 
 function getMapData(search) {
     $("#events > tbody").empty();
-    $("#brewerys > tbody").empty();
+    $("#breweries > tbody").empty();
     var url = "https://nominatim.openstreetmap.org/?format=json&limit=1&addressdetails=1&countrycodes=US&q="
     var queryTerm = '';
     for (let i = 0; i < search.length; i++) {
@@ -126,12 +126,12 @@ function getBreweriesByCity(city) {
                 $("<td><a target='_blank' href=\"" + response[i].website_url + "\" style=\"display:block;\">" + response[i].name + "</a></td>")
             );
             var newRow2 = $("<tr>").append(
-                $("<td>" + response[i].street + " " + response[i].postal_code + "</td>")
+                $("<td>Address: " + response[i].street + " " + response[i].postal_code + "</td>")
             );
             var newRow3 = $("<tr>").append(
-                $("<td>" + response[i].phone + "</td>")
+                $("<td style='border-bottom:1px solid #000;'>Phone: " + response[i].phone + "</td>")
             );
-            $("#brewerys").append(newRow1, newRow2, newRow3);
+            $("#breweries").append(newRow1, newRow2, newRow3);
         }
     });
 };
